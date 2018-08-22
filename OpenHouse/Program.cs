a1428eaae273fdd4dc517dc2959ef841fb0f7a01
Chip <chip.elmer@gmail.com>
2018-08-22 09:27:43 -0500

    Some small organizational changes to the Main method

--- OpenHouse/Program.cs
+++ OpenHouse/Program.cs
@@ -12,36 +12,44 @@ namespace OpenHouse
 			Realtor agent = new Realtor();
 			agent.Name = "Taj";
 
+            // Setup the kitchen
 			Room thekitchen = new Room();
 			thekitchen.Name = "The Kitchen";
 			thekitchen.Descriptions.Add("Is clean and white.");
 			thekitchen.Descriptions.Add("It's nice and big.");
-			Furniture sink = new Furniture();
+
+            Furniture sink = new Furniture();
 			sink.Descriptions.Add("The sink is very nasty.");
 			sink.Descriptions.Add("It has jets.");
-
-
-			Room thelivingroom = new Room();
-			thelivingroom.Name = "The Living Room";
-			thelivingroom.Descriptions.Add("It has plenty of room for furniture.");
-			Furniture sofa = new Furniture();
-			sofa.Descriptions.Add("It is really comfy");
-
-
-			Room masterbedroom = new Room();
-			masterbedroom.Name = "Master bedroom";
-			masterbedroom.Descriptions.Add("There is a big TV in here.");
-			Furniture TV = new Furniture();
-			TV.Descriptions.Add("It's a 4k UHD.");
-            TV.Descriptions.Add("It's a smart TV.");
+            thekitchen.Furnitures.Add(sink);
 
             house.Rooms.Add(thekitchen);
-			house.Rooms.Add(thelivingroom);
-			house.Rooms.Add(masterbedroom);
-			thekitchen.Furnitures.Add(sink);
-			thelivingroom.Furnitures.Add(sofa);
-			masterbedroom.Furnitures.Add(TV);
 
+
+            // Setup the living room
+            Room thelivingroom = new Room();
+			thelivingroom.Name = "The Living Room";
+			thelivingroom.Descriptions.Add("It has plenty of room for furniture.");
+
+            Furniture sofa = new Furniture();
+			sofa.Descriptions.Add("It is really comfy");
+            thelivingroom.Furnitures.Add(sofa);
+
+            house.Rooms.Add(thelivingroom);
+
+
+            // Setup the master bedroom
+            Room masterbedroom = new Room();
+			masterbedroom.Name = "Master bedroom";
+			masterbedroom.Descriptions.Add("There is a big TV in here.");
+
+            Furniture TV = new Furniture();
+			TV.Descriptions.Add("It's a 4k UHD.");
+            TV.Descriptions.Add("It's a smart TV.");
+            masterbedroom.Furnitures.Add(TV);
+
+			house.Rooms.Add(masterbedroom);
+			
 
 			agent.Speak($"Hello! Welcome to {house.Address} your future home. My name is {agent.Name}.");
 			
